1d67f09a1a92df256bf583662aaaa11021ddaf23
dhanagarajan <dhanagarajan@paypal.com>
2022-03-21 12:41:25 +0530

    Resteasy 4.x Changes

--- src/main/java/com/resteasy/tomcat/AsyncStreaming.java
+++ src/main/java/com/resteasy/tomcat/AsyncStreaming.java
@@ -1,11 +1,10 @@
 package com.resteasy.tomcat;
 
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.container.AsyncResponse;
-import jakarta.ws.rs.container.Suspended;
-import jakarta.ws.rs.core.Response;
-
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Response;
 import java.io.InputStream;
 import java.util.concurrent.CompletableFuture;
 
